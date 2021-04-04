180ec677078cf5f6de73901f5c810665b8db11d7
lkimilhol <lkimilhol@gmail.com>
2021-04-05 00:59:34 +0900

    메인함수 초기화

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,45 +3,5 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Solution.solution(new int[][]{{1, 2, 3, 5}, {5, 6, 7, 8}, {4, 3, 2, 1}}));
-    }
-}
-
-class Solution {
-    public static int[][] check;
-    public static int depth;
-    public static int breadth;
-    public static int sol;
-    public static int solution(int[][] land) {
-        depth = land.length;
-        breadth = land[0].length;
-        check = new int[depth][breadth];
-        dfs(land, 0, 0);
-        return sol;
-    }
-
-    public static void dfs(int[][] land, int n, int path) {
-        if (n == depth) {
-            int max = 0;
-            for (int i = 0; i < depth; i++) {
-                for (int j =0; j< breadth; j++) {
-                    if (check[i][j] == 1) {
-                        max += land[i][j];
-                    }
-                }
-            }
-            if (sol < max) {
-                sol = max;
-            }
-        } else {
-            for (int i = 0; i < breadth; i++) {
-                if (n > 0 && check[n - 1][i] == 1) {
-                    continue;
-                }
-                check[n][i] = 1;
-                dfs(land, n + 1, i);
-                check[n][i] = 0;
-            }
-        }
     }
 }
