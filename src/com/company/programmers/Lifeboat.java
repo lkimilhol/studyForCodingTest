a66eea26115f6d740cb20cea0aef1ca671487a2b
lkimilhol <lkimilhol@gmail.com>
2021-04-05 23:53:03 +0900

    빠진 내용 추가

--- src/com/company/programmers/Lifeboat.java
+++ src/com/company/programmers/Lifeboat.java
@@ -1,4 +1,25 @@
 package com.company.programmers;
 
+import java.util.Arrays;
+
 public class Lifeboat {
+    public int solution(int[] people, int limit) {
+        Arrays.sort(people);
+        int start = 0;
+        int cnt = 0;
+
+        for (int i = people.length - 1; i >= 0; i--) {
+            if (people[i] == 0) {
+                continue;
+            }
+
+            if (people[i] + people[start] <= limit) {
+                people[start] = 0;
+                start++;
+            }
+            cnt++;
+        }
+
+        return cnt;
+    }
 }
