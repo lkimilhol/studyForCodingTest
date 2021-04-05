49b9bbd4102c83b748d95c278f0aa9ca5fe2ca30
lkimilhol <lkimilhol@gmail.com>
2021-04-05 23:52:00 +0900

    빠진 내용 추가

--- src/com/company/programmers/NextBigNumber.java
+++ src/com/company/programmers/NextBigNumber.java
@@ -2,6 +2,24 @@ package com.company.programmers;
 
 public class NextBigNumber {
     public int solution(int n) {
-        return 0;
+        int inputCnt = getCount(n);
+
+        while (true) {
+            n++;
+            if (inputCnt == getCount(n)) {
+                break;
+            }
+        }
+        return n;
+    }
+
+    private int getCount(int n) {
+        int cnt = 0;
+        String str = Integer.toBinaryString(n);
+        int fromIndex = -1;
+        while ((fromIndex = str.indexOf("1", fromIndex + 1)) >= 0) {
+            cnt++;
+        }
+        return cnt;
     }
 }
