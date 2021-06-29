156ef503251402678f0c9c9487e01c7cb15952f1
lkimilhol <lkimilhol@gmail.com>
2021-06-29 23:03:48 +0900

    프로그래머스 스킬트리 수정

--- src/com/company/programmers/SkillTree.java
+++ src/com/company/programmers/SkillTree.java
@@ -6,20 +6,16 @@ import java.util.stream.Collectors;
 
 public class SkillTree {
     public int solution(String skill, String[] skill_trees) {
-        int answer = 0;
-        List<String> list = Arrays.stream(skill.split("")).collect(Collectors.toList());
+        int count = 0;
 
-        for (String s : skill_trees) {
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < s.length(); i++) {
-                if (list.contains(Character.toString(s.charAt(i)))) {
-                    sb.append(s.charAt(i));
-                }
-            }
-            if (skill.startsWith(sb.toString())) {
-                answer++;
+        for (String skill_tree : skill_trees) {
+            String reg = "[^" + skill + "]";
+            String newStr = skill_tree.replaceAll(reg, "");
+            if (skill.startsWith(newStr)) {
+                count++;
             }
         }
-        return answer;
+
+        return count;
     }
 }
