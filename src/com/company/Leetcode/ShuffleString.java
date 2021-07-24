815e03ae8d509ec84b139f4303886df5b5397f7c
lkimilhol <lkimilhol@gmail.com>
2021-07-25 00:45:15 +0900

    로직 개선

--- src/com/company/Leetcode/ShuffleString.java
+++ src/com/company/Leetcode/ShuffleString.java
@@ -6,17 +6,10 @@ import java.util.Map;
 
 public class ShuffleString {
     public String restoreString(String s, int[] indices) {
-        Map<Integer, String> strings = new HashMap<>();
-        for (int i = 0; i < indices.length; i++) {
-            strings.put(indices[i], Character.toString(s.charAt(i)));
+        char[] tmp = s.toCharArray();
+        for (int i = 0; i < s.length(); i++) {
+            tmp[indices[i]] = s.charAt(i);
         }
-        Object[] mapkey = strings.keySet().toArray();
-        Arrays.sort(mapkey);
-
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < strings.size(); i++) {
-            sb.append(strings.get(i));
-        }
-        return sb.toString();
+        return String.valueOf(tmp);
     }
 }
