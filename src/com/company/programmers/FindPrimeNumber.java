55bb1c7c27b4f1763e81144f426401a69fb014a0
lkimilhol <lkimilhol@gmail.com>
2022-03-23 21:42:21 +0900

    프로그래머스 소수 찾기 리팩토링중

--- src/com/company/programmers/FindPrimeNumber.java
+++ src/com/company/programmers/FindPrimeNumber.java
@@ -1,72 +1,58 @@
 package com.company.programmers;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class FindPrimeNumber {
-    static ArrayList<Integer> combNum;
-    static String[] numberArr;
+    private static final Set<Integer> PRIME = new HashSet<>();
 
     public static int solution(String numbers) {
-        final int size = 9999999 + 2;
-        final int[] primeNumbers = new int[size];
 
-        primeNumbers[0] = 1;
-        primeNumbers[1] = 1;
+        boolean[] visited = new boolean[numbers.length()];
 
-        numberArr = numbers.split("");
-
-        combNum = new ArrayList<>();
-        for(String s : numberArr){
-            combNum.add(Integer.parseInt(s));
+        for (int r = 1; r <= numbers.length(); r++) {
+            combination(numbers, visited, 0, r);
         }
 
-        int root = (int) Math.sqrt(primeNumbers.length);
-
-        for (int i = 2; i <= root; i++) {
-            if (primeNumbers[i] == 0) {
-                for(int j=i; i*j<size; j++) {
-                    primeNumbers[i * j] = i * j;
-                }
-            }
-        }
-
-        if(numberArr.length>1){
-            for(int i = 1;i<numberArr.length;i++){
-                int len = i+1;
-                int[] visit = new int[numberArr.length];
-                for(int j = 1;j<numberArr.length;j++){
-                    visit[j]=0;
-                }
-                searchComb("",visit,len);
-            }
-        }
-
-        Set<Integer> sol = new HashSet<>();
-        for(Integer i:combNum){
-            if (primeNumbers[i] == 0) {
-                sol.add(i);
-            }
-        }
-        return sol.size();
+        return PRIME.size();
     }
 
-    public static void searchComb(String ori,int[] visit,int len){
-        for(int i = 0;i<visit.length;i++){
-            if(visit[i] == 0){
-                var nOri = ori;
-                nOri += numberArr[i];
-                int[] newVisit = new int[visit.length];
-                System.arraycopy(visit,0,newVisit,0,visit.length);
-                newVisit[i] = 1;
-                if(nOri.length()<len){
-                    searchComb(nOri,newVisit,len);
-                }else{
-                    if(!combNum.contains(Integer.parseInt(nOri)))
-                        combNum.add(Integer.parseInt(nOri));
-                }
+    private static void combination(String numbers, boolean[] visited, int start, int r) {
+        if (r == 0) {
+            int number = parseInt(numbers, visited);
+            if (isPrime(number)) {
+                PRIME.add(number);
             }
         }
+
+        for (int i = start; i < numbers.length(); i++) {
+            visited[i] = true;
+            combination(numbers, visited, i + 1, r - 1);
+            visited[i] = false;
+        }
+    }
+
+    private static boolean isPrime(int number) {
+        if (number == 0) {
+            return false;
+        }
+
+        for (int i = 2; i <= (int) Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static int parseInt(String numbers, boolean[] visited) {
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < numbers.length(); i++) {
+            if (visited[i]) {
+                result.append(numbers.charAt(i));
+            }
+        }
+
+        return Integer.parseInt(result.toString());
     }
 }
