48a4f252b4ef91f21e9fe3aa25b940e8bd7499af
lkimilhol <lkimilhol@gmail.com>
2022-03-28 08:13:11 +0900

    프로그래머스 조이스틱 풀이

--- src/com/company/programmers/Joystick.java
+++ src/com/company/programmers/Joystick.java
@@ -2,53 +2,33 @@ package com.company.programmers;
 
 public class Joystick {
     public int solution(String name) {
-        int front = diff(name, 0) + 1;
-        int end = diff(name, 0) + 1;
+        int answer = 0;
 
-        String[] check = initCheckString(name);
+        int nextA;
+        int length = name.length();
+        int moveCount = length - 1;
 
-        for (int i = 1; i < name.length(); i++) {
-            front += diff(name, i);
-            check[i] = String.valueOf(name.charAt(i));
+        for (int i = 0; i < name.length(); i++) {
+            answer += findCharacter(name, i);
 
-            if (joinEquals(check, name)) {
-                break;
+            nextA = i + 1;
+
+            while(nextA < length && name.charAt(nextA) == 'A'){
+                nextA++;
             }
 
-            front++;
+            moveCount = Math.min(moveCount, i * 2 + length - nextA);
+            moveCount = Math.min(moveCount, (length - nextA) * 2 + i);
         }
 
-        check = initCheckString(name);
-
-        for (int i = name.length() - 1; i > 0; i--) {
-            end += diff(name, i);
-            check[i] = String.valueOf(name.charAt(i));
-
-            if (joinEquals(check, name)) {
-                break;
-            }
-
-            end++;
-        }
-
-        return Math.min(front, end);
+        return answer + moveCount;
     }
 
-    private int diff(String name, int i) {
+    private int findCharacter(String name, int i) {
         char c = name.charAt(i);
         int a = c - 'A';
         int b = 'Z' - c + 1;
 
         return Math.min(a, b);
-    }
-
-    private String[] initCheckString(String name) {
-        String[] split = "A".repeat(name.length()).split("");
-        split[0] = String.valueOf(name.charAt(0));
-        return split;
-    }
-
-    private boolean joinEquals(String[] check, String name) {
-        return String.join("", check).equals(name);
     }
 }
