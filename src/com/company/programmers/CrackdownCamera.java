f34c3cb3260315ee880f97f3f9fe614f8f7bc4b2
lkimilhol <lkimilhol@gmail.com>
2022-03-29 14:58:06 +0900

    프로그래머스 단속카메라 풀이

--- src/com/company/programmers/CrackdownCamera.java
+++ src/com/company/programmers/CrackdownCamera.java
@@ -11,16 +11,16 @@ public class CrackdownCamera {
         });
 
         int answer = 1;
-        int value = routes[0][1];
+        int camera = routes[0][1];
 
         for (int i = 1; i < routes.length; i++) {
             int[] route = routes[i];
 
-            if (route[0] <= value && value <= route[1]) {
+            if (route[0] <= camera && camera <= route[1]) {
                 continue;
             }
 
-            value = route[1];
+            camera = route[1];
             answer++;
         }
 
