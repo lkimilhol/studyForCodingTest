b1bd124477cdbe311cdf18d5c97b2efeae9fc398
lkimilhol <lkimilhol@gmail.com>
2022-03-31 18:04:58 +0900

    프로그래머스 디스크 컨트롤러 풀이

---
+++ src/com/company/programmers/DiscController.java
@@ -0,0 +1,31 @@
+package com.company.programmers;
+
+import java.util.Arrays;
+
+public class DiscController {
+    public int solution(int[][] jobs) {
+        Arrays.sort(jobs, (o1, o2) -> {
+            Integer a = o1[0] + o1[1];
+            Integer b = o2[0] + o2[1];
+            return a.compareTo(b);
+        });
+
+        int total = 0;
+        int time = 0;
+
+        for (int[] job : jobs) {
+            if (job[0] <= time) {
+                time += job[1];
+                total += time - job[0];
+                continue;
+            }
+
+            int i = job[0] - time;
+            total += i;
+            time += job[1];
+            total += time - job[0];
+        }
+
+        return total / jobs.length;
+    }
+}
