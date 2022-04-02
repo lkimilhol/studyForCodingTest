82399d0c378bbfd8401a8510ff2e4c953d33ee00
lkimilhol <lkimilhol@gmail.com>
2022-04-02 21:40:03 +0900

    릿코드 concatenation-of-array

--- src/com/company/Leetcode/ConcatenationOfArray.java
+++ src/com/company/Leetcode/ConcatenationOfArray.java
@@ -1,19 +1,14 @@
 package com.company.Leetcode;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class ConcatenationOfArray {
     public int[] getConcatenation(int[] nums) {
         int[] result = new int[nums.length * 2];
         int index = 0;
 
-        for (int i = 0; i < nums.length; i++) {
-            result[index++] = nums[i];
-        }
-
-        for (int i = 0; i < nums.length; i++) {
-            result[index++] = nums[i];
+        for (int num : nums) {
+            result[index] = num;
+            result[index + nums.length] = num;
+            index++;
         }
 
         return result;
