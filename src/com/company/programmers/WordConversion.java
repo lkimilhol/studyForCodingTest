e4a4aeb7ff4dcfe033afd1f6e362f77a5bf0a0cb
lkimilhol <lkimilhol@gmail.com>
2022-04-04 14:36:12 +0900

    프로그래머스 단어 변환

--- src/com/company/programmers/WordConversion.java
+++ src/com/company/programmers/WordConversion.java
@@ -1,32 +1,48 @@
 package com.company.programmers;
 
 public class WordConversion {
+    static boolean[] visited;
+    static int value = 51;
+
     public int solution(String begin, String target, String[] words) {
-        int answer = 0;
+        visited = new boolean[words.length];
 
-        boolean[] visit = new boolean[words.length];
-        for (int i = 0; i < words.length; i++) {
-            answer += dfs(words[i], target, words, visit, i);
-        }
+        dfs(begin, target, words, 0);
 
-        return answer;
-    }
-
-    private int dfs(String begin, String target, String[] words, boolean[] visit, int i) {
-        if (visit[i]) {
+        if (value == 51) {
             return 0;
         }
 
-        if ()
+        return value;
     }
 
-    private boolean isChangeable(String word, String target) {
-        int diff = 0;
+    private void dfs(String begin, String target, String[] words, int depth) {
+        if (begin.equals(target)) {
+            value = Math.min(value, depth);
+            return;
+        }
+        for (int i = 0; i < words.length; i++) {
+            if (!visited[i] && isChangeable(begin, words[i])) {
+                visited[i] = true;
+                dfs(words[i], target, words, depth + 1);
+                visited[i] = false;
+            }
 
-        for (int i = 0; i < word.length(); i++) {
-            diff += word.charAt(i) - target.charAt(i);
+        }
+    }
+
+    private boolean isChangeable(String begin, String target) {
+        int count = 0;
+
+        for (int i = 0; i < begin.length(); i++) {
+            if (begin.charAt(i) == target.charAt(i)) {
+                count++;
+            }
         }
 
-        return diff == 1;
+        if (count == begin.length() - 1) {
+            return true;
+        }
+        return false;
     }
 }
